97fec1f035ebdc9f26d2933008567936244aac4f
rongc <qiurongc@foxmail.com>
2021-07-05 09:53:55 +0800

    version update

--- app/src/main/java/com/rongc/navigation/OneFragment.kt
+++ app/src/main/java/com/rongc/navigation/OneFragment.kt
@@ -1,12 +1,9 @@
 package com.rongc.navigation
 
-import android.graphics.Color
 import android.os.Bundle
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.navigation.annotation.FragmentDestination
