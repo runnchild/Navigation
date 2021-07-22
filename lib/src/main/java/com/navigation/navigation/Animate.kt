f086684f85f37f7916db09c94b2bbffe14a6a5a5
qiurc <qiurongc@foxmail.com>
2021-07-22 23:35:22 +0800

    去除popAnim字段，

--- lib/src/main/java/com/navigation/navigation/Animate.kt
+++ lib/src/main/java/com/navigation/navigation/Animate.kt
@@ -1,8 +1,8 @@
 package com.navigation.navigation
 
-const val ANIM_NON = 1
-const val ANIM_DEFAULT = 2
-const val ANIM_POP = 3
+const val ANIM_DEFAULT = 0
+const val ANIM_NON = -1
+const val ANIM_POP = 1
 
 const val PAGE_TYPE_FRAGMENT = "fragment"
 const val PAGE_TYPE_ACTIVITY = "activity"
